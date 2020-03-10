ac119eba2f5c4179bf6808f5ec70604135414b36
Andrew Myers <andrew_myers@wgbh.org>
2020-03-10 10:00:41 -0400

    Config to use custom ActiveEncode engine adapter

--- config/initializers/active_encode.rb
+++ config/initializers/active_encode.rb
@@ -1,7 +1,10 @@
+require 'active_encode/engine_adapters/wgbh_skip_all_adapter'
 ActiveEncode::Base.engine_adapter = Settings.encoding.engine_adapter.to_sym
 case Settings.encoding.engine_adapter.to_sym
 when :ffmpeg
   MasterFile.default_encoder_class = FfmpegEncode
+when :wgbh_skip_all
+  MasterFile.default_encoder_class = WGBHSkipAllEncode
 when :matterhorn
   Rubyhorn.init
 when :elastic_transcoder
