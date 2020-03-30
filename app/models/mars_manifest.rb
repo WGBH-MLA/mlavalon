2537aba86455fe4a6a418cead8939ea5f0f597bb
Andrew Myers <andrew_myers@wgbh.org>
2020-03-30 15:24:46 -0400

    Creates MarsManifest model
    MarsManifest is a basic PORO model that uses ActiveModel::Validations to
    encaspsulate validation of the MARS export CSV manifest used as input for the
    Avalon Ingest API. Also provides accessors to the data within the CSV so we
    don't have to deal with arrays and hashes so much.
    
    * Adds specs for validation on URL, CSV-ness, and proper CSV headers
    * Adds custom Rspec matcher have_errors_on for checking ActiveModel::Validations
      errors.
    * Separates hash_match mather to it's own file.
    * Fixes error in headers of sample_csv_ingest_1.csv.
    
    Closes #84.

---
+++ app/models/mars_manifest.rb
@@ -0,0 +1,182 @@
+# MarsManifest is a basic PORO model that uses ActiveModel::Validations to
+# encaspsulate validation of the MARS export CSV manifest used as input for the
+# Avalon Ingest API. Also provides accessors an enumerable array of
+# MarsManifestRow objects, each of which are used to validate individual row
+# data.
+class MarsManifest
+  include ActiveModel::Validations
+
+  attr_reader :url
+
+  validates :rows, presence: true
+  validates :headers, presence: true
+  validate :valid_headers?
+
+  def initialize(url:)
+    @url = url
+  end
+
+  def headers
+    @headers ||= Array(csv&.first)
+  end
+
+  def rows
+    @rows ||= csv&.map do |row|
+      # TODO: code it
+      # MarsManifestRow.new(headers: headers, row_data: row)
+    end
+  end
+
+  private
+
+    # Parses raw data as CSV, memoized in @csv. If an error occurs, it adds
+    # the error message on the :csv field, invalidating the model instance.
+    # @return Array parsed CSV data; nil if an error occcurs.
+    def csv
+      @csv ||= CSV.parse(raw_data)
+    rescue => e
+      add_error(:csv, e.message)
+      nil
+    end
+
+    # Fetches data form the URL in the @url attribute, memoized in @raw_data. If
+    # an error occurs, it adds the error message on the :url field, invalidating
+    # the model instance.
+    # @return String the raw data fetched from the URL in the @url attribute;
+    #   nil if an error occurs.
+    def raw_data
+      @raw_data ||= Net::HTTP.get(URI.parse(url))
+    rescue => e
+      add_error(:url, e.message)
+      nil
+    end
+
+    # Runs validation on each row.
+    # @return [Boolean] true if all rows are valid.
+    def rows_valid?
+      return true
+      # TODO: replace true with validation of all rows, e.g...
+      # rows.map { |row| row.valid? }.all?
+    end
+
+    # Adds an error message to a field idempotently (because errors.add is not
+    # idempotent, and you can end up with a field having duplicate errors).
+    # @param field [Symbol] the field name.
+    # @param msg [String] the error message
+    def add_error(field, msg)
+      errors.add(field, msg) unless errors[field].include? msg
+    end
+
+    # @return [Boolean] true if #headers are valid; false if not.
+    def valid_headers?
+      if missing_headers?
+        add_error(:headers, "Missing headers '#{missing_headers.join("','")}'")
+      end
+
+      if unrecognized_headers?
+        add_error(:headers, "Unrecognized headers '#{unrecognized_headers.join("', '")}'")
+      end
+      # return false unless headers
+      # regular_headers, *file_header_groups = headers.slice_when { |i, j| headers_eq?(i, j) }.to_a
+      # headers_eq?(regular_headers, HEADERS) &&
+      #   file_header_groups.map { |file_headers| headers_eq?(file_headers, FILE_HEADERS) }
+    end
+
+    # Checks for missing headers from the CSV manifest by normalizing them and
+    # then comparing them to the normalized required headers.
+    # @return [Array] list of missing headers.
+    def missing_headers
+      normalized_headers = headers.map { |h| normalize_header(h) }
+      @missing_headers ||= MarsManifest.required_headers.select do |req_header|
+        normalized_headers.exclude? normalize_header(req_header)
+      end
+    end
+
+    # Checks for unrecognized headers from the CSV manifest by normalizing them
+    # and then comparing them to the normalized required headers.
+    # @return [Array] list of unrecognized headers.
+    def unrecognized_headers
+      normalized_required_headers = MarsManifest.required_headers.map { |h| normalize_header(h) }
+      @unrecognized_headers ||= headers.select do |header|
+        normalized_required_headers.exclude? normalize_header(header)
+      end
+    end
+
+    # @return [Boolean] true if there are missing headers; false if not.
+    def missing_headers?; !missing_headers.empty?; end
+
+    # @return [Boolean] true if there are unercognized headers; false if not.
+    def unrecognized_headers?; !unrecognized_headers.empty?; end
+
+    # Normalizes a header string by 1) making lowercase, 2) reducing excessive
+    # whitespace down to a single space, 3) stripping leading/trailing
+    # whitespace.
+    # @return [String] the normalized header.
+    def normalize_header(header)
+      header.downcase.gsub(/ +/, ' ').strip
+    end
+
+  # MarsManfiest class methods
+  class << self
+    def required_headers
+      [
+        "Collection Name",
+        "Collection Description",
+        "Unit Name",
+        "Collection ID",
+        "Title",
+        "Date Issued",
+        "Creators",
+        "Alternative Titles",
+        "Translated Titles",
+        "Uniform Titles",
+        "Statement Of Responsibility",
+        "Date Created",
+        "Copyright Date",
+        "Abstract",
+        "Notes",
+        "Format",
+        "Resource Types",
+        "Contributors",
+        "Publishers",
+        "Genres",
+        "Subjects",
+        "Related Item Urls",
+        "Geographic Subjects",
+        "Temporal Subjects",
+        "Topical Subjects",
+        "Bibliographic Id",
+        "Languages",
+        "Terms Of Use",
+        "Tables Of Contents",
+        "Physical Description",
+        "Other Identifiers",
+        "Comments",
+        "File Label",
+        "File Title",
+        "Instantiation Label",
+        "Instantiation Id",
+        "Instantiation Streaming URL",
+        "Instantiation Duration",
+        "Instantiation Mime Type",
+        "Instantiation Audio Bitrate",
+        "Instantiation Audio Codec",
+        "Instantiation Video Bitrate",
+        "Instantiation Video Codec",
+        "Instantiation Width",
+        "Instantiation HeightFile Location",
+        "File Checksum",
+        "File Size",
+        "File Duration",
+        "File Aspect Ratio",
+        "File Frame Size",
+        "File Format",
+        "File Date Digitized",
+        "File Caption Text",
+        "File Caption Type",
+        "File Other Id",
+        "File Comment"
+      ]
+    end
+  end
+end
---
+++ spec/models/mars_manifest_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+require 'mars_manifest'
+
+RSpec.describe MarsManifest do
+  subject { described_class.new url: url }
+
+  describe '#validate' do
+    context 'when created with no URL' do
+      let(:url) { nil }
+      before { subject.validate }
+      it { is_expected.to have_errors_on(:url) }
+    end
+
+    context 'when created with an invalid URL' do
+      let(:url) { 'not a url' }
+      before { subject.validate }
+      it { is_expected.to have_errors_on(:url) }
+    end
+
+    context 'when created with a valid URL', :focus do
+      let(:url) { 'http://example.com' }
+      before do
+        # Now that we're dealing with a valid URL, stub the request to it.
+        stub_request(:get, url).
+          with(
+            headers: {
+              'Accept'=>'*/*',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Host'=>'example.com',
+              'User-Agent'=>'Ruby'
+            }
+          ).
+          to_return(status: 200, body: url_response_body, headers: {})
+
+        # Call MarsManifest#validate before each example.
+        subject.validate
+      end
+
+      context 'and when the URL resolves to non-CSV data' do
+        let(:url_response_body) { File.read('./app/assets/images/favicon.ico') }
+        it { is_expected.to have_errors_on(:csv) }
+      end
+
+      context 'and when the URL resolves to actual CSV data' do
+        context 'and when the CSV data has invalid headers' do
+          let(:url_response_body) { "Not,the,right,headers" }
+          it { is_expected.to have_errors_on(:headers) }
+        end
+
+        context 'and when the CSV data has the correct headers' do
+          let(:url_response_body) { File.read('./spec/fixtures/sample_csv_ingest/sample_csv_ingest_1.csv') }
+          it { is_expected.not_to have_errors_on(:headers) }
+        end
+      end
+    end
+  end
+end
