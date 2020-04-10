b2849a5f19bbe88796220772e4f723ced09b13c7
Henry Fog <foglaboratories@gmail.com>
2020-04-10 12:30:08 -0700

    Adds MarsIngestItem model
    * Adds MarsIngestItem model to track progress of jobs that are ingesting
      individual rows of MARS manifest.
    * Adds specs, factory, and migrations for MarsIngestItem model.
    * Adds db schema changes.
    
    Also,
    * Adds Demo lib. Use Demo.user to get the woo@foo.edu.
    * Fixes bad header in sample CSV.
    * Fixes bad list of acceptable headers in MarsManifest model.

---
+++ app/services/collection_creator.rb
@@ -0,0 +1,11 @@
+class CollectionCreator
+
+  def self.find_or_create_collection(collection_name, unit_name, collection_desc, manager_email='archivist1@example.com')
+    collection = Admin::Collection.where(name_uniq_si: collection_name).first
+
+    return collection if collection
+    Admin::Collection.create({name: collection_name, unit: unit_name, description: collection_desc, managers: [manager_email]})
+  end
+
+
+end
