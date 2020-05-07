7c37407b5df52bb61fef16d247d8aa8d57f3c9b4
Andrew Myers <andrew_myers@wgbh.org>
2020-05-07 10:52:31 -0400

    Fix mars ingest spec

--- spec/models/mars_ingest_spec.rb
+++ spec/models/mars_ingest_spec.rb
@@ -3,25 +3,24 @@ require 'rails_helper'
 describe MarsIngest do
 
   describe 'validation' do
+    subject { FactoryBot.build(:mars_ingest) }
+
+    # Verify subject valid before hand and test validity after specific changes.
+    before { expect(subject).to be_valid }
+
     it 'validates the presence of :manifest_url' do
-      mars_ingest = FactoryBot.build(:mars_ingest, manifest_url: nil)
-      mars_ingest.validate
-      expect(mars_ingest.errors[:manifest_url].first).to match /required/
+      subject.manifest_url = nil
+      subject.validate
+      expect(subject).to have_error_on :manifest_url, /required/
     end
 
     context 'when the manifest is invalid, with errors' do
-      let(:manifest) { instance_double(MarsManifest) }
       let(:manifest_errors) {
         { foo: ["Alpha", "Bravo"], bar: ["Charlie", "Delta"] }
       }
 
       before do
-        # Mock the "nearest edge" within #validate_manifest.
-        allow(manifest).to receive(:valid?).and_return(false)
-        allow(manifest).to receive(:errors).and_return(manifest_errors)
-        allow(subject).to receive(:manifest).and_return(manifest)
-
-        # Call the method under test.
+        allow(subject.manifest).to receive(:errors).and_return(manifest_errors)
         subject.validate
       end
 
