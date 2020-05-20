45db709f549222acbff8cffdd32df707e9d32556
Andrew Myers <andrew_myers@wgbh.org>
2020-05-20 12:33:07 -0400

    Don't stub HOST header for mars manifest urls.

--- spec/models/mars_manifest_spec.rb
+++ spec/models/mars_manifest_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe MarsManifest do
             headers: {
               'Accept'=>'*/*',
               'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'Host'=>'example.com',
               'User-Agent'=>'Ruby'
             }
           ).
