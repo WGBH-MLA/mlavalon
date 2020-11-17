718ff9aa73ed7d1ac89ef447c6e6950b4f44d88c
foglabs <foglaboratories@gmail.com>
2020-11-17 12:40:53 -0500

    'puts not log'

--- app/services/mars_ingest_watcher.rb
+++ app/services/mars_ingest_watcher.rb
@@ -1,14 +1,14 @@
 class MarsIngestWatcher
   def self.run
-    Rails.logger.info "Hello!"
+    puts  "Hello!"
     key = get_manifest
 
     if key
-      Rails.logger.info "Got a manifest at #{key}, lets go!"
+      puts  "Got a manifest at #{key}, lets go!"
       url = move_manifest(key)
       run_manifest(url)
     else 
-      Rails.logger.info "It aint no manifests.. Bye!"
+      puts  "It aint no manifests.. Bye!"
     end
   end
 
@@ -21,7 +21,7 @@ class MarsIngestWatcher
   end
 
   def self.get_manifest
-    Rails.logger.info "Let me check those manifests for you..."
+    puts  "Let me check those manifests for you..."
     # list unprocessed s3 folder
     client = get_s3_client
     objs = client.list_objects({bucket: "mlavalon", prefix: "ingestion-inbox"})
@@ -32,7 +32,7 @@ class MarsIngestWatcher
       # ingest first one
       # url = "https://mlavalon.s3.amazonaws.com/export_2020-06-24_300_allQCd-httpsfix-SMALLCUT.csv"
       return nil unless csv_key
-      Rails.logger.info "I got #{csv_key}"
+      puts  "I got #{csv_key}"
       # move ingest to processed s3 folder
 
     return csv_key
@@ -47,16 +47,16 @@ class MarsIngestWatcher
     # destination = "https://mlavalon.s3.amazonaws.com/ingestion-outbox/" + File.basename(key)
 
     output_key = "ingestion-outbox/" + File.basename(input_key)
-    Rails.logger.info "Now copying #{input_key} to #{output_key}..."
+    puts  "Now copying #{input_key} to #{output_key}..."
     client.copy_object({bucket: "mlavalon", key: output_key, copy_source: "mlavalon/" + input_key})
-    Rails.logger.info "Checking for presence of #{output_key}"
+    puts  "Checking for presence of #{output_key}"
     if client.head_object({bucket: "mlavalon", key: output_key})
       # 404 if not
 
       # need to make copy public so that ingest machinery can download the manifest
-      Rails.logger.info "Adding public acl to #{output_key}"
+      puts  "Adding public acl to #{output_key}"
       client.put_object_acl({ acl: "public-read", bucket: "mlavalon", key: output_key })
-      Rails.logger.info "Now deleting #{input_key}..."
+      puts  "Now deleting #{input_key}..."
       client.delete_object({bucket: "mlavalon", key: input_key })
 
       # tis is the (now public) url we're actually going to hit for the ingest
@@ -71,11 +71,11 @@ class MarsIngestWatcher
 
     mi = MarsIngest.new(manifest_url: url, submitter_id: user.id)
     if mi.save
-      Rails.logger.info "Ok, looks like the manifest was valid!"
+      puts  "Ok, looks like the manifest was valid!"
       
       mi.mars_ingest_items.each do |mars_ingest_item|
         job_id = MarsIngestItemJob.perform_later(mars_ingest_item.id)
-        Rails.logger.info("Started MarsIngestItemJob with jid #{job_id} from MarsIngestItem #{mars_ingest_item.id}")
+        puts ("Started MarsIngestItemJob with jid #{job_id} from MarsIngestItem #{mars_ingest_item.id}")
       end
     else
       require('pry');binding.pry
