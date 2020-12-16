0ca0055ccd1da1dc1991fabe7c4dfea86e093625
foglabs <foglaboratories@gmail.com>
2020-12-16 13:43:01 -0500

    'log all collections'

--- app/services/collection_creator.rb
+++ app/services/collection_creator.rb
@@ -5,6 +5,10 @@ class CollectionCreator
     # TODO: check normalization method that fills in name_uniq_si field on Collection create
 
     Rails.logger.info "XXXXXX PARAMS - #{collection_name} #{unit_name} #{collection_desc} #{manager_user_key}"
+
+    allc = Admin::Collection.all
+    Rails.logger.info "XXXXXXX #{allc}"
+
     collection = Admin::Collection.where(name_uniq_si: collection_name.to_s.downcase.gsub(/\s/, '')).first
     Rails.logger.info "XXXXXX EXISTIN COLLECTION #{collection}"
 
@@ -13,6 +17,7 @@ class CollectionCreator
       c=Admin::Collection.create!({name: collection_name, unit: unit_name, description: collection_desc, managers: [manager_user_key]})
     rescue Exception => e
       Rails.logger.error "XXXXXXXXX FAILED TO CREATE #{e.message} #{e.backtrace}"
+      false
     end
   end
 end
