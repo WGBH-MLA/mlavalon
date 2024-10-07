b9e9d58b1032038ee52ed975ed9461eb32ec9521
fog <foglaboratories@gmail.com>
2024-10-07 16:55:23 -0400

    'fix env export for ingest'

--- script/watch_folder.sh
+++ script/watch_folder.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+rm -f v.txt
+
 # get em
 # all=$(cat /proc/1/environ)
 all=$(xargs -0 -L1 -a /proc/1/environ)
 
+
+
 # loop through, bash auto splits on whitespace ooh ooh
 for var in $all
 do
+  # # export em
+  # piece2=${var#*=}
+  # piece1=${var%"$piece2"}
+  # eval 'export $piece1"$piece2"'
 
-  # export em
-  piece2=${var#*=}
-  piece1=${var%"$piece2"}
-
-  eval 'export $piece1"$piece2"'
-  # eval 'export $piece1"${piece2//-D/\\\-D}"'
-
+  echo $var >> v.txt
 done
+
+export $(grep -v '^#' v.txt | xargs -d '\n')
 
 # ooh ah so nice
 
